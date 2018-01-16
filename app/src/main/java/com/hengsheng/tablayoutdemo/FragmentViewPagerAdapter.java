ecaeb87a0551335a4abbd2e403a6ffa729c51ecd
zb <zhangb@henghaojijin.com>
2018-01-16 16:07:28 +0800

    带图标的tablayout

---
+++ app/src/main/java/com/hengsheng/tablayoutdemo/FragmentViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.hengsheng.tablayoutdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by zhangb on 2018/1/16/016
+ */
+
+class FragmentViewPagerAdapter extends FragmentPagerAdapter{
+
+    private List<Fragment> fragments;
+    private List<String> titles;
+
+    public FragmentViewPagerAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+        super(fm);
+        this.fragments = fragments;
+        this.titles = titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+}
